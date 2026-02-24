4fef3fd831dd4dca6987e403ea990eed7384e790
David Bertoin <davidbertoin@gmail.com>
2026-02-24 23:10:14 +0100

    Revert "[Algorithm] LPIPS loss"

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -3,6 +3,6 @@ from algorithm.repa import REPA
 from algorithm.tread import Tread
 from algorithm.sprint import SPRINT
 from algorithm.contrastive_flow_matching import ContrastiveFlowMatching
-from algorithm.lpips import LPIPS
 
-__all__ = ["EMA", "REPA", "Tread", "SPRINT", "ContrastiveFlowMatching", "LPIPS"]
+
+__all__ = ["EMA", "REPA", "Tread", "SPRINT", "ContrastiveFlowMatching"]
