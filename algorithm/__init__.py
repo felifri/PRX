819347ae31e129f1cb762d4d2f83e94ff91e3b66
David Bertoin <davidbertoin@gmail.com>
2026-02-27 09:11:36 +0000

    add LPIPS in __init__.py

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -3,7 +3,8 @@ from algorithm.repa import REPA
 from algorithm.tread import Tread
 from algorithm.sprint import SPRINT
 from algorithm.contrastive_flow_matching import ContrastiveFlowMatching
+from algorithm.lpips import LPIPS
 from algorithm.perceptual_dino import PerceptualDINO
 
 
-__all__ = ["EMA", "REPA", "Tread", "SPRINT", "ContrastiveFlowMatching", "PerceptualDINO"]
+__all__ = ["EMA", "REPA", "Tread", "SPRINT", "ContrastiveFlowMatching", "PerceptualDINO", "LPIPS"]
