6d005ac26a4280cd00ebc95cf030fe666e922ff3
David Bertoin <davidbertoin@gmail.com>
2026-03-05 10:03:49 +0000

    Fix bare imports in algorithm __init__ to use prx.* paths

--- prx/algorithm/__init__.py
+++ prx/algorithm/__init__.py
@@ -1,10 +1,10 @@
-from algorithm.ema import EMA
-from algorithm.repa import REPA
-from algorithm.tread import Tread
-from algorithm.sprint import SPRINT
-from algorithm.contrastive_flow_matching import ContrastiveFlowMatching
-from algorithm.lpips import LPIPS
-from algorithm.perceptual_dino import PerceptualDINO
+from prx.algorithm.ema import EMA
+from prx.algorithm.repa import REPA
+from prx.algorithm.tread import Tread
+from prx.algorithm.sprint import SPRINT
+from prx.algorithm.contrastive_flow_matching import ContrastiveFlowMatching
+from prx.algorithm.lpips import LPIPS
+from prx.algorithm.perceptual_dino import PerceptualDINO
 
 
 __all__ = ["EMA", "REPA", "Tread", "SPRINT", "ContrastiveFlowMatching", "PerceptualDINO", "LPIPS"]
